cffb029f77e0bd9f2bdf61c61843b47b6a28352b
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:32:14 +0200

    Representar el conocimiento

---
+++ 2023_05_13_0_18_09_taxonomias_categorias_y_objetos.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 01:25:05 2023
+
+@author: Gadiel Jimenez
+"""
+
+
+#Taxonomías: Categorías y Objetos
+
+#Definimos la taxonomía como un diccionario
+taxonomia = {
+    'Animales': {
+        'Mamíferos': {
+            'Perro': {},
+            'Gato': {},
+            'Elefante': {}
+        },
+        'Aves': {
+            'Águila': {},
+            'Pato': {},
+            'Pingüino': {}
+        },
+        'Reptiles': {
+            'Serpiente': {},
+            'Cocodrilo': {},
+            'Tortuga': {}
+        }
+    },
+    'Plantas': {
+        'Árboles': {
+            'Roble': {},
+            'Pino': {},
+            'Arce': {}
+        },
+        'Flores': {
+            'Rosa': {},
+            'Lirio': {},
+            'Tulipán': {}
+        }
+    }
+}
+
+#Función para mostrar la taxonomía
+def mostrar_taxonomia(taxonomia, nivel=0):
+    for categoria, subcategorias in taxonomia.items():
+        print('  ' * nivel + '- ' + categoria)
+        if subcategorias:
+            mostrar_taxonomia(subcategorias, nivel + 1)
+
+mostrar_taxonomia(taxonomia)
